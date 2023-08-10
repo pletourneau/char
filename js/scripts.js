fb47153e70dbb247464e34d9e708c9998eaf2320
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-10 13:08:56 -0700

    add basic rendering of form

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,25 @@ function handleSubmission(event) {
 
 
   const operator = document.querySelector("input[name='mythRace']:checked").value;
-
+  let abilities = document.querySelectorAll("input[name='ability']:checked");
+  let values = [];
+  abilities.forEach((ability) => {
+    values.push(ability.value);
+  });
+  console.log(values);
+ 
+  
+  
+ 
  
 
-  document.getElementById("output").innerText = operator;
   
+  let result = operator + "with" + values;
+  document.getElementById("output").innerText = result;
 }
 
 window.addEventListener("load", function() {
   const form = document.getElementById("char");
   form.addEventListener("submit", handleSubmission);
+  
 });
